c6a57d69c217891ce78849d39adb4dbd65e7ac0c
Stugr <nick@stugr.com>
2020-06-20 15:10:43 +1000

    Replaced servo with stepper and rewrote most of it

--- BlindsAutomation.ino
+++ BlindsAutomation.ino
@@ -1,78 +1,127 @@
-#include <Servo.h>
-
-Servo servo;
+#include <Stepper.h>
+#include <Bounce2.h>
 
 const int buttonOpenPin = 2;
 const int buttonClosePin = 3;
-const int servoPin = 9;
 const int lightPin = A0;
 const int lightThreshold = 450;
 const int darkThreshold = 500;
+const int ledPin = 13;
 
-int buttonOpenState = 0;
-int buttonCloseState = 0;
+const int steps = 2048; // motor 28BYJ-48
+const int openSteps = -(steps * 5);
+const int closedSteps = 0;
+
+int ledState = HIGH;
 bool dark = false;
+int requestedPosition = 0;
+bool currentlyMoving = false;
 
 int position = 0;
 
-void setup() {
-  // attach servo and set its range
-  servo.attach(servoPin, 1000, 2000); // TODO: adjust range depending on servo used, and see if default start position can be avoided
+Stepper stepper(steps, 8, 10, 9, 11);
 
+const int buttonCount = 2;
+const int debounceDelay = 50;
+const int buttonPins[buttonCount] = {buttonOpenPin, buttonClosePin};
+
+Bounce buttons[buttonCount];
+
+void setup() {
   // setup buttons as inputs
-  pinMode(buttonOpenPin, INPUT);
-  pinMode(buttonClosePin, INPUT);
+  //pinMode(buttonOpenPin, INPUT);
+  //pinMode(buttonClosePin, INPUT);
+
+  for (int i = 0; i < buttonCount; i++) {
+      buttons[i].attach(buttonPins[i], INPUT);
+      buttons[i].interval(debounceDelay);
+  }
+
+  pinMode(ledPin, OUTPUT);
+
+  stepper.setSpeed(10);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  buttonOpenState = digitalRead(buttonOpenPin);
-
-  if (buttonOpenState == HIGH) {
-    rotateClockwise();
-  } else {
-    // nothing
+  // update debouncers
+  for (int i = 0; i < buttonCount; i++) {
+    buttons[i].update();
+  }
+  
+  if (buttons[0].rose() || buttons[1].rose()) {
+    if (currentlyMoving) {
+      Serial.println("CANCEL button pressed");
+      requestedPosition = position;
+      currentlyMoving = false;
+    } else {
+      // yellow
+      if (buttons[0].rose()) {
+        Serial.println("Close button pressed");
+        //rotateClockwise();
+        requestedPosition = closedSteps;
+      }
+      // red
+      else if (buttons[1].rose()) {
+        Serial.println("Open button pressed");
+        //rotateCounterClockwise();
+        requestedPosition = openSteps;
+      }
+    }
+  }
+  // next loop through after button press
+  else {
+    if (requestedPosition > position) {
+      rotateStepper(2048/16);
+    } else if (requestedPosition < position) {
+      rotateStepper(-2048/16);
+    } else {
+      currentlyMoving = false;
+    }
   }
 
-  buttonCloseState = digitalRead(buttonClosePin);
-
-  if (buttonCloseState == HIGH) {
-    rotateCounterClockwise();
-  } else {
-    // nothing
-  }
+  
 
   int lightLevel = analogRead(lightPin);
   if (lightLevel > darkThreshold && !dark) {
     dark = true;
-    Serial.println("dark");
-    Serial.println(lightLevel);
-    rotateCounterClockwise();
+    //Serial.println("dark");
+    //Serial.println(lightLevel);
+    //rotateCounterClockwise();
   } else if (lightLevel < lightThreshold & dark ) {
     dark = false;
-    Serial.println("light");
-    Serial.println(lightLevel);
-    rotateClockwise();
+    //Serial.println("light");
+    //Serial.println(lightLevel);
+    //rotateClockwise();
   }
+  
+  //lightLevel = map(lightLevel, 0, 1023, 0, 180);
 }
 
+
 void rotateClockwise() {
-  rotateServo(180);
+  rotateStepper(2048/16);
 }
 
 void rotateCounterClockwise() {
-  rotateServo(0);
+  rotateStepper(-2048/16);
 }
 
-void rotateServo(int pos) {
-  if (position != pos) {
-    Serial.print("Moving from ");
-    Serial.print(position);
+void rotateStepper(int pos) {
+  //if (position != pos) {
+    currentlyMoving = true;
+    position += pos;
+    Serial.print("Moving ");
+    Serial.print(pos);
     Serial.print(" to ");
-    Serial.println(pos);
-    position = pos;
-    servo.write(position);
-    delay(15);
-  }  
+    Serial.print(position);
+    Serial.print(" (requested position is ");
+    Serial.print(requestedPosition);
+    Serial.print(" and current light level is ");
+    Serial.print(analogRead(lightPin));
+    Serial.println(")");
+    stepper.step(pos);
+    //delay(15);
+  //}  
 }
